27293369686be79f140c6f883aa8af8bb026ee13
void main() <100292340+Webwizards60@users.noreply.github.com>
2023-03-14 22:35:52 +0530

    📝 Updated Navbar Functionalities

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import logo from '../assets/Logo.png'
-import NavBar from '../json/navBar.json';
+import React from 'react';
+import logo from '../assets/Logo.png';
 import { ButtonPrimary } from '../Reusable';
 import { NavLink, Link } from 'react-router-dom';
-import '../../public/Styles/navBar.scss'
+import '../../public/Styles/navBar.scss';
 
 const Navbar = () => {
     return (
         <div >
             <div className="navBar">
-                <div className="logoSec">
-                    <img src={logo} alt="Decode_Logo" />
-                    <span>DECODE</span>
-                </div>
+                    <Link to={"/"} className="logo" >
+                        <img src={logo} alt="Decode_Logo" />
+                        <span>DECODE</span>
+                    </Link>
                 <div className="navLinks">
-                    {
-                        NavBar.map(
-                            navbar => {
-                                return (
-                                    <ul>
-                                        <li>
-                                            <NavLink activeClassName='active' className='nav__link' to={navbar.link} >{navbar.title}</NavLink>
-                                        </li>
-                                    </ul>
-                                )
-                            }
-                        )
-                    }
+                    <ul>
+                        <li>
+                            <NavLink activeClassName='active' className='nav__link' to={"/about"} >About</NavLink>
+                        </li>
+                        <li>
+                            <a className='nav__link'  >Questions</a>
+                        </li>
+                        <li>
+                            <a className='nav__link'  >Contact</a>
+                        </li>
+                    </ul>
                 </div>
                 <div className="ButtonSec">
                     <Link to={'/login'} className="login_button">LOGIN</Link>
@@ -34,7 +31,7 @@ const Navbar = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
